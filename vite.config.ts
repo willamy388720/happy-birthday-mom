96c4877e5a99dfc7f03399cf05f991531bc4f591
willamy388720 <willamygt@gmail.com>
2025-01-29 04:46:39 -0300

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import * as path from "node:path";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
